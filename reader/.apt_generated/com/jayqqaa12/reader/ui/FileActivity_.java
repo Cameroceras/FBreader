0bc2e71e9586246b8691a8d9f5172fa9eeb87143
12 <476335667@qq.com>
2014-09-25 16:55:36 +0800

    1234

--- reader/.apt_generated/com/jayqqaa12/reader/ui/FileActivity_.java
+++ reader/.apt_generated/com/jayqqaa12/reader/ui/FileActivity_.java
@@ -74,13 +74,13 @@ public final class FileActivity_
 
     @Override
     public void onViewChanged(HasViews hasViews) {
-        iv_head_left = ((ImageView) hasViews.findViewById(id.iv_head_left));
         iv_head_right = ((ImageView) hasViews.findViewById(id.iv_head_right));
+        iv_head_left = ((ImageView) hasViews.findViewById(id.iv_head_left));
         tv_head_logo = ((TextView) hasViews.findViewById(id.tv_head_logo));
         tv_head = ((TextView) hasViews.findViewById(id.tv_head));
+        elv = ((ExpandableListView) hasViews.findViewById(id.elv));
         tv = ((TextView) hasViews.findViewById(id.tv));
         iv_empty = ((ImageView) hasViews.findViewById(id.iv_empty));
-        elv = ((ExpandableListView) hasViews.findViewById(id.elv));
         {
             View view = hasViews.findViewById(id.iv_head_left);
             if (view!= null) {
